5b7cf24aec44cac33bc9dee5c229c336fb3e8506
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-23 21:54:49 +0100

    init

---
+++ app/src/main/java/com/example/templo/on1_employee.java
@@ -0,0 +1,14 @@
+package com.example.templo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class on1_employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on1_employee);
+    }
+}
