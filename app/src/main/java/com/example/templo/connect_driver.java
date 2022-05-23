c47e8cf2125c88ce24c11951b92dbd629de27f1a
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-23 22:06:31 +0100

    init

---
+++ app/src/main/java/com/example/templo/connect_driver.java
@@ -0,0 +1,14 @@
+package com.example.templo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class connect_driver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_driver);
+    }
+}
---
+++ app/src/main/java/com/example/templo/on2_employee.java
@@ -0,0 +1,14 @@
+package com.example.templo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class on2_employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on2_employee);
+    }
+}
