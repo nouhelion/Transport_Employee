9d43076fb5f59310d36e770539c18ccd093af1ed
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-25 15:57:39 +0100

    create employee

---
+++ app/src/main/java/com/example/templo/historique_employee.java
@@ -0,0 +1,14 @@
+package com.example.templo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class historique_employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historique_employee);
+    }
+}
---
+++ app/src/main/java/com/example/templo/navigation_bar_employee.java
@@ -0,0 +1,14 @@
+package com.example.templo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class navigation_bar_employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_bar_employee);
+    }
+}
