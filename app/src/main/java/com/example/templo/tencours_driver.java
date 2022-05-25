7f74d90667a849e37823695fed4d092fc4c7ec3e
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-25 23:56:02 +0100

    profil nav bar

--- app/src/main/java/com/example/templo/tencours_driver.java
+++ app/src/main/java/com/example/templo/tencours_driver.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class trajet_ncours_driver extends AppCompatActivity {
+public class tencours_driver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trajet_ncours_driver);
+        setContentView(R.layout.activity_tencours_driver);
     }
 }
