a1ec0b456d9c8e5d650e6427821ebdf5800489dc
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-06-04 04:30:26 +0100

    connection done

--- app/src/main/java/com/example/templo/connect_driver.java
+++ app/src/main/java/com/example/templo/connect_driver.java
@@ -1,25 +1,94 @@
 package com.example.templo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class connect_driver extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class connect_driver extends AppCompatActivity implements View.OnClickListener {
 
     ImageView back;
+    Button login;
+    private EditText editTextemail, editTextpassword;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_driver);
-        back=(ImageView) findViewById(R.id.back);
+        back = (ImageView) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(connect_driver.this,MainActivity.class);
+                Intent intent = new Intent(connect_driver.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        //firebase code
+
+        editTextemail = (EditText) findViewById(R.id.in1);
+        editTextpassword = (EditText) findViewById(R.id.in2);
+
+        mAuth = FirebaseAuth.getInstance();
+        login = (Button) findViewById(R.id.cont);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.cont:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email = editTextemail.getText().toString().trim();
+        String password = editTextpassword.getText().toString().trim();
+        if (email.isEmpty()) {
+            editTextemail.setError("l'email est obligatoire");
+            editTextemail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextemail.setError("entrez un email valid");
+            editTextemail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextpassword.setError("le mot de passe est obligatoire");
+            editTextpassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            editTextpassword.setError("le mot de passe est invalide");
+            editTextpassword.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    //redirect to welcome
+                    Intent intent = new Intent(connect_driver.this, tencours_driver.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(connect_driver.this, "failed to connect! try again!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
