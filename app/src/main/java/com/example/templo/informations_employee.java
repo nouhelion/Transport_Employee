3064e7f33be8ce4f10aa254d6a0c268a372d2644
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-06-06 21:58:09 +0100

    show name

--- app/src/main/java/com/example/templo/informations_employee.java
+++ app/src/main/java/com/example/templo/informations_employee.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
--- app/src/main/java/com/example/templo/navigation_bar_employee.java
+++ app/src/main/java/com/example/templo/navigation_bar_employee.java
@@ -1,14 +1,29 @@
 package com.example.templo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class navigation_bar_employee extends AppCompatActivity {
     ImageView info,accueil,reclamations,rate;
+    private TextView name;
+    // creating a variable for firebasefirestore.
+    private FirebaseUser user;
+    FirebaseFirestore db= FirebaseFirestore.getInstance();
+    DocumentReference documentReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,5 +61,34 @@ public class navigation_bar_employee extends AppCompatActivity {
             }
         });
 
+       name=(TextView) findViewById(R.id.nam);
+
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentuid=user.getUid();
+        DocumentReference reference;
+        FirebaseFirestore firestore=FirebaseFirestore.getInstance();
+        reference=firestore.collection("Employe").document(currentuid);
+
+        reference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.getResult().exists()){
+                            String Nom,Prenom;
+                            Nom=task.getResult().getString("nom");
+                            Prenom=task.getResult().getString("prenom");
+
+                            name.setText(Nom+" "+Prenom);
+
+                        }
+
+                    }
+                });
+    }
+
 }
